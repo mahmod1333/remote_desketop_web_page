ba26d03ba3cb3456f20b657ec5ef9fc960863a92
mahmod1333 <mahmod.ayman.okasha@gmail.com>
2023-06-02 20:43:59 +0300

    moor good

--- controllers/client.controllers.js
+++ controllers/client.controllers.js
@@ -1,8 +1,8 @@
 const path = require('path')
 const ClientPage = (req,res)=>{
-    res.sendFile(path.resolve("Pages/client/ClientPage/ClientPage.html"))
+    res.sendFile(path.resolve("src/Pages/client/ClientPage/ClientPage.html"))
      }
      const ClientOnActivePage = (req,res)=>{
-        res.sendFile(path.resolve("Pages/client/ClientOnActive/ClientOnActivePage.html"))
+        res.sendFile(path.resolve("src/Pages/client/ClientOnActive/ClientOnActivePage.html"))
        } 
        module.exports ={ClientPage,ClientOnActivePage}
--- controllers/server.controllers.js
+++ controllers/server.controllers.js
@@ -1,9 +1,9 @@
 const path = require('path')
 const ServerPage = (req,res)=>{
-    res.sendFile(path.resolve("Pages","server","ServerPage","ServerPage.html"))
+    res.sendFile(path.resolve("src/Pages","server","ServerPage","ServerPage.html"))
      }
      const ServerOnActivePage = (req,res)=>{
-        res.sendFile(path.resolve("Pages","server","ServerOnActive","ServerOnActivePage.html"))
+        res.sendFile(path.resolve("src/Pages","server","ServerOnActive","ServerOnActivePage.html"))
        }
 
        module.exports={ServerPage,ServerOnActivePage}
